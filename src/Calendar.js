80ee93723c0c0ef3d834f7e44cdb594f035f7197
Aaron <rogersac24@gmail.com>
2019-09-05 17:26:05 +0500

    adding google calendar api

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,17 +1,64 @@
 import React, { Component } from 'react';
 import { getDay, getDaysInMonth, startOfMonth } from 'date-fns';
 import './Calendar.css';
+import { gapi } from 'gapi-script';
+
 class Calendar extends Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
 			displayMonth: this.props.month,
-			displayYear: this.props.year
+			displayYear: this.props.year,
+			events: []
 		};
 		this.handleData = this.handleData.bind(this);
 		this.handleDays = this.handleDays.bind(this);
 		this.handleMonth = this.handleMonth.bind(this);
 	}
+
+	// Google Calendar API
+
+	componentDidMount = () => {
+		this.getEvents();
+	};
+
+	getEvents() {
+
+		let that = this;
+		function start() {
+			gapi.client
+				.init({
+					apiKey: 'AIzaSyDrKBMhLmh1-M3ubOcxqLQuhQrHEx2_U8Y'
+				})
+				.then(function() {
+					return gapi.client.request({
+						path: `https://www.googleapis.com/calendar/v3/calendars/${`68d19lho66i1dklnm3e7lpg7fs@group.calendar.google.com`}/events`
+					});
+				})
+				.then(
+					(response) => {
+						let events = response.result.items;
+						that.setState(
+							{
+								events
+							},
+							() => {
+								
+								console.log(that.state.events);
+								
+							}
+						);
+					},
+					function(reason) {
+						console.log(reason);
+					}
+				);
+		}
+		gapi.load('client', start);
+	}
+
+	//End Google Calendar API
 
 	handleData(event) {
 		const { name, value } = event.target;
@@ -72,6 +119,7 @@ class Calendar extends Component {
 					value={this.state.displayMonth === 0 ? 'minusOneYear' : 'dec'}
 					onClick={this.handleData}
 				/>
+
 				<h3>
 					{months[this.state.displayMonth]} {this.state.displayYear}
 				</h3>
@@ -89,10 +137,10 @@ class Calendar extends Component {
 		let date = new Date(year, month, 1);
 		let days = [];
 		let daysUI;
-		let startDay = getDay(startOfMonth(new Date(year, month))) + 1;
+		let startDay = getDay(startOfMonth(new Date(year, month)));
 
 		const styles = {
-			gridColumnStart: 0
+			gridColumnStart: startDay
 		};
 		let numDays = getDaysInMonth(new Date(year, month));
 		for (let i = 0; i < numDays; i++) {
