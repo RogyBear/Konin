b183c26b1a0e6ee28377b004c9aa091dba6f44d5
Aaron <rogersac24@gmail.com>
2019-09-18 13:17:03 +0500

    changing links

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
-import Carousel from './Carousel/Carousel';
+// import Carousel from './Carousel/Carousel';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root3'));
-ReactDOM.render(<Carousel />, document.getElementById('root2'));
+// ReactDOM.render(<Carousel />, document.getElementById('root2'));
